edd70751636b05341913f6f94daf8c6fe4f52bd4
YumeNoKage <ericopratama2003@gmail.com>
2022-09-15 19:39:42 +0700

    update for modal delete dasboard

--- src/components/dashboard/List.vue
+++ src/components/dashboard/List.vue
@@ -1,47 +1,4 @@
 <template>    
-    <!-- modal delete activity -->
-    <div class="modal fade" id="deleteActivity" tabindex="-1" :aria-labelledby="`deleteActivityLabel`" aria-hidden="true" data-cy="modal-delete">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content rounded-12 border-0">
-                <div class="modal-body border-0">
-                    <div class="pt-4">
-                        <img class="mx-auto d-block" src="../../assets/icons/warning.svg" alt="warning" width="84" data-cy="modal-delete-icon">
-                    </div>
-                    <div class="desc text-center my-4" data-cy="modal-delete-title">
-                        <span class="font-18 fw-custome-500 text-custome-black">
-                            Apakah anda yakin menghapus activity
-                        </span> 
-                        <br>
-                        <span class="font-18 fw-custome-700 text-custome-black">
-                            “{{activityName}}”?
-                        </span>
-                    </div>
-                </div>
-                <div class="modal-footer justify-content-center border-0">
-                    <button type="button" class="btn bg-custome-gray-shoft rounded-pill px-4 font-18 text-custome-gray-strong fw-custome-600" id="closeModalDelete" data-bs-dismiss="modal" data-cy="modal-delete-cancel-button">Close</button>
-                    <button type="button" v-show="false" class="btn bg-custome-gray-shoft rounded-pill px-4 font-18 text-custome-gray-strong fw-custome-600" data-bs-dismiss="modal" data-cy="activity-item-delete-button">Close</button>
-                    <button type="button" class="btn bg-custome-red rounded-pill px-4 font-18 text-white fw-custome-600" @click="deleteActivity(`activity-groups/${idActivity}`)" data-cy="modal-delete-confirm-button">Hapus</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- modal information -->
-    <div class="modal fade" id="deleteActivityNotif" tabindex="-1" aria-labelledby="deleteActivityNotifLabel" aria-hidden="true" data-cy="modal-information">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content rounded-12 border-0">
-                <div class="modal-body border-0">
-                    <div class="desc text-left">
-                        <span class="font-14 fw-custome-500 text-custome-black">
-                            <img src="../../assets/icons/info.svg" class="to-red">
-                            Activity berhasil {{deleteAction ? 'dihapus' : 'ditambahkan'}}
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="row">
         <div class="col-12 d-flex justify-content-center mt-5 pt-3" v-if="data.length == 0 && !isLoading" data-cy="activity-empty-state">
             <img src="../../assets/illustration/activity-empty-state.svg" alt="activity empty state" data-cy="activity-empty-state">
@@ -60,9 +17,25 @@
                             </div>
                         </div>
                     </router-link>
-                    <button class="p-0 border-0 bg-transparent position-absolute bottom-0 end-0 pe-1 mx-4 mb-3" data-bs-target="#deleteActivity" data-bs-toggle="modal" @click="idActivity = item.id, activityName = item.title, deleteAction = true" data-cy="activity-item-delete-button">
+                    <button class="p-0 border-0 bg-transparent position-absolute bottom-0 end-0 pe-1 mx-4 mb-3" data-bs-target="#deleteActivity" data-bs-toggle="modal" @click="updateToParent(item.id, item.title), deleteAction = true" data-cy="activity-item-delete-button">
                         <img src="../../assets/icons/trash.svg" alt="delete" width="24">
                     </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- modal information -->
+    <div class="modal fade" id="deleteActivityNotif" tabindex="-1" aria-labelledby="deleteActivityNotifLabel" aria-hidden="true" data-cy="modal-information">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content rounded-12 border-0">
+                <div class="modal-body border-0">
+                    <div class="desc text-left">
+                        <span class="font-14 fw-custome-500 text-custome-black">
+                            <img src="../../assets/icons/info.svg" class="to-red">
+                            Activity berhasil {{deleteAction ? 'dihapus' : 'ditambahkan'}}
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -85,6 +58,8 @@
                 deleteAction: true,
             }
         },
+
+        emits:['delete:Activity'],
         
         methods: {
             ...mapActions({
@@ -104,26 +79,9 @@
                 }
             },
 
-            async deleteActivity(url, params){
-                this.isLoading = true
-
-                let data = {url, params}
-                const response = await this.deleteData(data)
-
-                if (response.status == 200) {
-                    document.getElementById('closeModalDelete').click()
-                    $('#deleteActivityNotif').modal('show')
-                    this.getActivity('activity-groups')
-                    
-                    setTimeout(() =>{
-                        $('#deleteActivityNotif').modal('hide')
-                    },3000)
-
-                }
-            },
-
-            toDetail(item){
-                this.$router.push(`/detail/${item.id}`)
+            updateToParent(id, name){
+                let data ={id, name}
+                this.$emit('delete:Activity', data)
             }
         },
 
--- src/views/Dasboard.vue
+++ src/views/Dasboard.vue
@@ -12,7 +12,33 @@
         </div>
         <!-- list activity -->
         <div class="col-12">
-            <List :showLoading="isLoading" ref="ListActivity"/>
+            <List :showLoading="isLoading" ref="ListActivity" @delete:Activity="openModalDelete($event)"/>
+        </div>
+    </div>
+
+    <!-- modal delete activity -->
+    <div class="modal fade" id="deleteActivity" tabindex="-1" :aria-labelledby="`deleteActivityLabel`" aria-hidden="true" data-cy="modal-delete">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content rounded-12 border-0">
+                <div class="modal-body border-0">
+                    <div class="pt-4">
+                        <img class="mx-auto d-block" src="../assets/icons/warning.svg" alt="warning" width="84" data-cy="modal-delete-icon">
+                    </div>
+                    <div class="desc text-center my-4" data-cy="modal-delete-title">
+                        <span class="font-18 fw-custome-500 text-custome-black">
+                            Apakah anda yakin menghapus activity
+                        </span> 
+                        <br>
+                        <span class="font-18 fw-custome-700 text-custome-black">
+                            “{{activityName}}”?
+                        </span>
+                    </div>
+                </div>
+                <div class="modal-footer justify-content-center border-0">
+                    <button type="button" class="btn bg-custome-gray-shoft rounded-pill px-4 font-18 text-custome-gray-strong fw-custome-600" id="closeModalDelete" data-bs-dismiss="modal" data-cy="modal-delete-cancel-button">Close</button>
+                    <button type="button" class="btn bg-custome-red rounded-pill px-4 font-18 text-white fw-custome-600" @click="deleteActivity(`activity-groups/${idActivity}`)" data-cy="modal-delete-confirm-button">Hapus</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -26,12 +52,15 @@
         data(){
             return{
                 isLoading: false,
+                activityName: null,
+                idActivity: null,
             }
         },
 
         methods: {
             ...mapActions({
-                postData: 'postData'
+                postData: 'postData',
+                deleteData: 'deleteData'
             }),
     
             async postActivity(url, params){
@@ -45,9 +74,35 @@
                     this.$refs.ListActivity.deleteAction = false
                     $('#deleteActivityNotif').modal('show')
                     this.$refs.ListActivity.getActivity('activity-groups')
-                    setTimeout( ()=>{$('#deleteActivityNotif').modal('hide')}, 2000)
+                    setTimeout( ()=>{$('#deleteActivityNotif').modal('hide')}, 1000)
                 }
-            }
+            },
+
+            openModalDelete(data){
+                this.activityName = data.name;
+                this.idActivity = data.id;
+
+                $('#deleteActivity').modal('show')
+            },
+
+            async deleteActivity(url, params){
+                this.isLoading = true
+
+                let data = {url, params}
+                const response = await this.deleteData(data)
+
+                if (response.status == 200) {
+                    $('#deleteActivity').modal('hide')
+                    $('#deleteActivityNotif').modal('show')
+
+                    this.$refs.ListActivity.getActivity('activity-groups')
+                    
+                    setTimeout(() =>{
+                        $('#deleteActivityNotif').modal('hide')
+                    },1000)
+
+                }
+            },
         },
     }
         
