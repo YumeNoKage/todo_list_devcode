afef9f75edc63e6f3e8016ef062ab299c791fac5
YumeNoKage <ericopratama2003@gmail.com>
2022-09-16 08:10:02 +0700

    reset form add todo

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -129,7 +129,7 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" data-cy="modal-add-close-button"></button>
                     </div>
                     <div class="modal-body px-0">
-                        <div>
+                        <form @submit.prevent="todo.id ? updateTodo() : addItem()" data-cy="modal-add-save-button">
                             <div class="mb-3 modal-body py-0">
                                 <label class="form-label font-custome-12 fw-custome-600 text-custome-black" data-cy="modal-add-name-title">NAMA LIST ITEM</label>
                                 <input v-model="todo.title" type="text" class="form-control font-custome-16 fw-custome-400 text-custome-black px-3 py-3" id="inputNameItem" placeholder="Tambahkan nama list item" data-cy="modal-add-name-input" required>
@@ -165,9 +165,9 @@
                                 </div>
                             </div>
                             <div class="modal-footer">
-                                <button class="btn bg-custome-blue-shoft rounded-pill px-3 text-white font-custome-18 fw-custome-600" type="button" @click="todo.id ? updateTodo() : addItem()" data-cy="modal-add-save-button" :disabled="todo.title == null || todo.title == '' ? true : false">Simpan</button>
+                                <button class="btn bg-custome-blue-shoft rounded-pill px-3 text-white font-custome-18 fw-custome-600" type="submit" :disabled="todo.title == null || todo.title == '' ? true : false">Simpan</button>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
