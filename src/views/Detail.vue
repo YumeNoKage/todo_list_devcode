c87d9550face6b0d4ca55ba6b3b2c8e057c86953
YumeNoKage <ericopratama2003@gmail.com>
2022-09-17 19:47:48 +0700

    update element for title detail todo

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -30,8 +30,8 @@
                     <a href="/" class="border-0 text-decoration-none text-custome-black" data-cy="todo-back-button">
                         <img src="../assets/icons/back-left.svg" alt="back">
                     </a>
-                    <input type="text" id="editTitleTodo" v-model="data.title" :class="`border-0 ${editTitle ? 'border-bottom' : ''} fw-custome-700`" @change="updateTitle(), editTitle = false" @click="editTitle = true" @blur="editTitle = false" style="outline: none;" data-cy="todo-title" :readonly="!editTitle" focusable>
-                    <!-- <label v-show="!editTitle" @click="editTitle = !editTitle" for="editTitleTodo" :data-cy="!editTitle ? 'todo-title' : ''"><h1 class="font-custome-36 fw-custome-700">{{data.title}}</h1></label> -->
+                    <input v-if="editTitle" type="text" id="editTitleTodo" v-model="data.title" :class="`border-0 ${editTitle ? 'border-bottom' : ''} fw-custome-700`" @change="updateTitle(), editTitle = false" @click="editTitle = true" @blur="editTitle = false" style="outline: none;" data-cy="todo-title" :readonly="!editTitle" focusable>
+                    <label v-if="!editTitle" @click="editTitle = !editTitle" for="editTitleTodo" :data-cy="!editTitle ? 'todo-title' : ''"><h1 class="font-custome-36 fw-custome-700">{{data.title}}</h1></label>
                     <span class="ms-3">
                         <button class="bg-transparent border-0" @click="editTitle = !editTitle" data-cy="todo-title-edit-button">
                             <img width="24" src="../assets/icons/pencil.svg" alt="edit">
